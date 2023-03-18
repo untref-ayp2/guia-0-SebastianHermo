12b32bdb9a380d71199a4c9806c10c3bdd05ff92
SebastianHermo <sebastianhermoavila@gmail.comm>
2023-03-18 09:21:41 -0300

    Numeros Primos y Final de Estructuras Repetitivas

--- 02-Estructuras_Repetitivas/funciones/factorial.go
+++ 02-Estructuras_Repetitivas/funciones/factorial.go
@@ -10,3 +10,19 @@ func Factoreo(n int) int {
 func SumasSucesivas(a int, b int) int {
 	return ((a * (a + 1)) / 2) * ((b * (b + 1)) / 2)
 }
+
+func EsPrimo(a int) bool {
+	resultado := 1
+	NumeroPrimo := false
+	numerosPrimos := [5]int{a, 2, 3, 5, 7}
+
+	for i := 0; i < 5; i++ {
+		if a%numerosPrimos[i] == 0 {
+			resultado = resultado + 1
+		}
+	}
+	if resultado == 2 {
+		NumeroPrimo = true
+	}
+	return NumeroPrimo
+}
--- 02-Estructuras_Repetitivas/main/main.go
+++ 02-Estructuras_Repetitivas/main/main.go
@@ -6,6 +6,13 @@ import (
 )
 
 func main() {
-	fmt.Println("Factorial de 5 es:", funciones.Factoreo(5))
+	fmt.Println("El Factoreo de 5 es:", funciones.Factoreo(5))
+	fmt.Println("El Factoreo de 10 es:", funciones.Factoreo(10))
+	fmt.Println("El Factoreo de 0 es:", funciones.Factoreo(0))
 	fmt.Println("El producto de 3 y 6 es:", funciones.SumasSucesivas(3, 6))
+	fmt.Println("El producto de 0 y 10 es:", funciones.SumasSucesivas(0, 10))
+	fmt.Println("El producto de 10 y 100 es:", funciones.SumasSucesivas(10, 100))
+	fmt.Println("¿El 9 es primo?", funciones.EsPrimo(9))
+	fmt.Println("¿El 11 es primo?", funciones.EsPrimo(11))
+	fmt.Println("¿El 1 es primo?", funciones.EsPrimo(1))
 }
